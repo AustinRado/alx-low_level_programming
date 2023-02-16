22ee115e76041a7d73cdb7d85a2b7956fd9d226f
AustinRado <radoaussy@gmail.com>
2023-02-16 08:41:21 -0800

    program that prints and returns a stringfollowed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Serves as the entry point for the program that prints a line with puts function
- * 
+ * main - Serves as the entry point for the program
  * Return: 0 (Success)
  */
- int main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
