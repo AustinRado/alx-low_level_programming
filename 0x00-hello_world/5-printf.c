f2193b37798e8cc9a86aa9d9518c5d93370146eb
AustinRado <radoaussy@gmail.com>
2023-02-16 08:49:44 -0800

    print stdout using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include < stdio.h >
+
+/**
+ *main - serves as entry point of the program
+ *prints to the stdout using printf()
+ *return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
