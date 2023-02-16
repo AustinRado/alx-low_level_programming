0dd463dae513b175e8400ae76c4f4a530afa7ccf
AustinRado <radoaussy@gmail.com>
2023-02-16 07:16:40 -0800

    c program that prints followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Serves as the entry point for the program
+ * main(void) - is a function that expects no arguments. returns an integer representing the application software status.
+ * puts() function writes a string to stout up to but not including the null character. A newline character is appended to the output.
+ * Return: 0 (Success)
+ */
+ int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+returns(0);
+}
