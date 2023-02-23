9780a435f5892716af66c418a6933921492583b5
AustinRado <radoaussy@gmail.com>
2023-02-23 10:58:08 -0800

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * *file - main.h
+ * Austhor: Austin Rado
+ * Desc: Header contains prototypes for all functions
+ * used in this directory
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
