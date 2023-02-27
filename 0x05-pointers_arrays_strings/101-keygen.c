1172ba2db34fa338cdafa05b29f47e6a64576734
AustinRado <radoaussy@gmail.com>
2023-02-27 12:18:49 -0800

    generate kygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,27 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - generates keygen.
+* Return: Always 0.
+*/
+
 int main(void)
 {
+int ran = 0, i = 0;
+time_t second;
 
-	return (0);
+srand((unsigned int) time(&second));
+
+while (i < 2772)
+{
+	ran = rand() % 128;
+	if ((i + ran) > 2772)
+		break;
+	i = i + ran;
+	printf("%c", ran);
+}
+printf("%c\n", (2772 - i));
+return (0);
 }
