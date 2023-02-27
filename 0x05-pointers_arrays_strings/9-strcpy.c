146cc1fce30581d8bcf8f396c18dca1c7433aa48
AustinRado <radoaussy@gmail.com>
2023-02-27 12:27:25 -0800

    init commit, locally tested all file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
- */
+* *_strcpy - copy the string pointed to by src including the terminating
+* null byte to the buffer pointed to by dest
+* @dest: value to check
+* @src: value to check
+*
+* Return: pointer to dest
+*/
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -13,5 +17,6 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
