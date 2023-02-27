f6ae8ddfc867108b9a02095627ad248474c0b866
AustinRado <radoaussy@gmail.com>
2023-02-27 11:33:58 -0800

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,7 +13,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char src);
-char *_strcpy(char *dest, char* src);
-int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif
