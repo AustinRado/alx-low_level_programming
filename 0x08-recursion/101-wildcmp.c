6d27bdbc1270e979ed78973d8e372561738176f5
AustinRado <radoaussy@gmail.com>
2023-03-08 10:21:29 -0800

    Initial commit, functionally tested

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* wildcmp - compare two string and return 1 if identical else 0
+* @s1: value to check
+* @s2: vlaue to check
+*
+* Return: 1 if identical else 0
+*/
+
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
