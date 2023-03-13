5357e99e3554d39a86d67cbc2c899fa368cc61a3
AustinRado <radoaussy@gmail.com>
2023-03-12 22:43:15 -0700

    inital commit, functionally tested

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* *create_array -  create an array of chars and initialzes
+* it with a specific char.
+* @c: char t initialize to
+* @size: number of bytes to allocate
+*
+* Return: a pointer to array or null if it fails
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size);
+
+	if (size == 0 || array == 0)
+		return (NULL);
+	while (size--)
+		array[size] = c;
+	return (array);
+}
