eece5d8e6305a09f27b32b34eb55410973544496
AustinRado <radoaussy@gmail.com>
2023-03-12 22:52:49 -0700

    initital commit, functionally tested

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* *_strdup - copies the string given as paramter
+* @str: string to duplicate
+*
+* Return: pointer to the copies string(SUCESS), NULL (Error)
+*/
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
+}
