ce2d2bccfa514fffad19b8bd15bf4cd44c5e50b9
AustinRado <radoaussy@gmail.com>
2023-04-07 23:12:14 -0700

    initial commit, locally tested

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 0x12345678;
+	char *ptr = (char *)&num;
+
+	if (*ptr == 0x78)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
