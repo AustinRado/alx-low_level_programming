846ab0a57cb9b9ae7d82975289974f38df6b7954
AustinRado <radoaussy@gmail.com>
2023-04-14 15:31:20 -0700

    initial commit, locally tested

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,21 +27,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdt < 0)
+	file_from = read(fdf, buf, BUF_SIZE);
+	file_to = write(fdt, buf, file_from);
+	while (file_from > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		close(fdf);
-		exit(99);
-	}
-	while ((file_from = read(fdf, buf, sizeof(buf))) > 0)
-	{
-		file_to = write(fdt, buf, file_from);
-		if (file_to < 0 || file_to != file_from)
+		if (file_to != file_from || file_to < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fdf);
+			exit(99);
 		}
 	}
+	if (file_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 		close_ff = close(fdf);
 		close_ft = close(fdt);
 		if (close_ff < 0 || close_ft < 0)
